957d4bf2cd6be364a49775a051006e2138c34d66
erick6aelgg <erick6aelgg@ciencias.unam.mx>
2026-07-14 15:33:52 -0600

    Código de la clase 7: - Explicación de la entrada por teclado en Java con el uso de Scanner. - Ejemplo de POO: Clase Estudiante con atributos y métodos básicos.

---
+++ codigos/clase7/Entrada.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in); 
+        String nombre = scan.nextLine();
+        System.out.println("¡Hola " + nombre + "!");
+    
+    }
+}
---
+++ codigos/clase7/Estudiante.java
@@ -0,0 +1,66 @@
+public class Estudiante {
+    
+    // Características de un estudiante de la UNAM
+    // Nombre, apellidos, num de cuenta
+    // Edad, carrera, semestre, inscrito
+    // calificaciones
+
+
+    // Atributos
+    private String nombre;
+    private String apellidos;
+    private String numDeCuenta; // Lo dejamos como String dado que no operaremos con el
+    private int edad; // Por ejemplo, 18
+    private String carrera; // Ejemplos: Ciencias de la computación, matemáticas, fisica, actuaria
+    private int semestre; // Por ejemplo: 2, podemos hacer comparaciones > <
+    private boolean inscrito; // Esta inscrito? -> true/false
+    private int[] calificaciones; // No tiene tamaño asignado, puede ser incluso [10, 9, 10, 8, 10]
+
+    // Métodos constructores
+    // Constructor por omisión
+    public Estudiante(){}
+
+    // Constructor por parámetros
+    public Estudiante(String nombre, String apellidos, String numDeCuenta, int edad, String carrera, int semestre, boolean inscrito, int[] calificaciones){
+        System.out.println(this.nombre);
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.numDeCuenta = numDeCuenta;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.semestre = semestre;
+        this.inscrito = inscrito;
+        this.calificaciones = calificaciones;
+    }
+
+    // Métodos setters y getters
+    // Setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+
+    // Otros setters para los demás atributos
+
+    // Getters
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String getApellidos(){
+        return this.apellidos;
+    }
+
+    // Otros getters para los demás atributos
+
+
+    // Métodos adicionales
+    public static void presentarse(){
+        System.out.println("Hola soy " + nombre + " " + apellidos);
+    }
+
+
+}
